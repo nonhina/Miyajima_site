6b654dc072c730a4cf884c011453cc0ef8bcd133
HinanoMiyajima <hinanomiyajima@HinanoMiyajimanoMacBook-ea.local>
2018-09-25 18:07:04 +0900

    変更

--- home.js
+++ home.js
@@ -1,12 +1,31 @@
-var loop = setInterval(function() {
-    var clone = $(".photo li:first").clone(true);
-    $(".photo li:first").animate({
-    marginLeft : "-300px"
-    }, {
-    duration : 1000,
-    complete : function() {
-        $(".photo li:first").remove();
-        clone.clone(true).insertAfter($(".photo li:last"));
-    }
-    });
-}, 2500);
+var windowWidth = $(window).width();
+var windowSm = 640;
+if (windowWidth <= windowSm) {
+  var loop = setInterval(function() {
+      var clone = $(".photo li:first").clone(true);
+      $(".photo li:first").animate({
+      marginLeft : "-150px"
+      }, {
+      duration : 1000,
+      complete : function() {
+          $(".photo li:first").remove();
+          clone.clone(true).insertAfter($(".photo li:last"));
+      }
+      });
+  }, 2500);
+
+} else {
+  var loop = setInterval(function() {
+      var clone = $(".photo li:first").clone(true);
+      $(".photo li:first").animate({
+      marginLeft : "-300px"
+      }, {
+      duration : 1000,
+      complete : function() {
+          $(".photo li:first").remove();
+          clone.clone(true).insertAfter($(".photo li:last"));
+      }
+      });
+  }, 2500);
+
+}
